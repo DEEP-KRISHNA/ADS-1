bd32f93c89cfb93f3880c62c935714ab42bdcc48
Sandy-35 <sandy.krish1995@gmail.com>
2019-11-21 12:16:55 +0530

    commit proj 2.0

--- PROJECT 3/PercolationStats.java
+++ PROJECT 3/PercolationStats.java
@@ -1,33 +1,68 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdOut;
+
 public class PercolationStats {
 
-    // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials) {
+    private double percoconlo;
+    private double percoconhi;
+    private double percomean;
+    private double percostddev;
+    private double[] percosta;
 
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int m, int trials) {
+        int n = m;
+        int t = trials;
+        if (n <= 0 || t <= 0) {
+            throw new IllegalArgumentException();
+        }
+        percosta = new double[t];
+        for (int i = 0; i < t; i++) {
+            Percolation perco = new Percolation(n);
+            while (perco.percolates() == false) {
+                int c = StdRandom.uniform(1, n + 1);
+                int r = StdRandom.uniform(1, n + 1);
+                if (perco.isOpen(r, c) == false) {
+                    perco.open(r, c);
+                }
+            }
+            int opsites = perco.numberOfOpenSites();
+            double temp = (double) opsites / (n * n);
+            percosta[i] = temp;
+        }
+
+        percomean = StdStats.mean(percosta);
+        percostddev = StdStats.stddev(percosta);
+        percoconhi = percomean + (1.96 * percostddev / Math.sqrt(t));
+        percoconlo = percomean - (1.96 * percostddev / Math.sqrt(t));
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return 0.0;
+        return percomean;
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return 0.0;
+        return percostddev;
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return 0.0;
+        return percoconlo;
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return 0.0;
+        return percoconhi;
     }
 
     // test client (see below)
     public static void main(String[] args) {
-
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, t);
     }
 
 }
